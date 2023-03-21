5280a921b4d753b929998ec67ae81013adf44900
Rehema <rehemachiona05@gmail.com>
2023-03-21 15:06:39 -0700

    I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - print all alphabet in lower case
+ * print_alphabet - print all alphabet in lower case
+ * main - Entry point
  * Return: Always 0
  */
 void print_alphabet(void)
