62c209c7cc805faea0c15276bbe8fc2a85ebe8b6
Rehema <rehemachiona05@gmail.com>
2023-04-06 16:15:00 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt(int a, int b);
 int is_prime_number(int n);
+int _is_prime(int a, int b);
 
 #endif
