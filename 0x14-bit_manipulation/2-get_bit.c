1154a90981730eb08a675227ca82a1cba1ec6b93
Reh05 <rehemachiona05@gmail.com>
2023-07-06 15:24:43 -0700

    10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,17 +7,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask = 1UL << index; /* one is a the desired index */
+	unsigned long int mask = 0;
 
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	if ((n & mask) != 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	mask = 1UL << index;
+
+	return ((n & mask) != 0);
 }
