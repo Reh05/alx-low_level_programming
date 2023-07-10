59303db035fb5603b1854a80d079fba12696f4c8
Reh05 <rehemachiona05@gmail.com>
2023-07-10 13:37:17 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,10 @@ int create_file(const char *filename, char *text_content)
 	jk = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 06);
 
 	if (jk == -1 || filename == NULL)
+		return (-1);
+	if (text_content == NULL)
 		return (1);
+
 	write(jk, text_content, strlen(text_content));
 
 	close(jk);
